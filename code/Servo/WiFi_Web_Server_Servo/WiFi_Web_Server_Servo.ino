d54f9d40639a3f5e3f4aaa8ca2086ba2c61ff2ce
romain <romain@funtronic.co.uk>
2020-09-16 17:43:31 +0200

    Fixed example to use ESP32Servo library. THe Arduino library <Servo.h> does not support ESP32. Need to install ESP32Servo from the library manager.

--- code/Servo/WiFi_Web_Server_Servo/WiFi_Web_Server_Servo.ino
+++ code/Servo/WiFi_Web_Server_Servo/WiFi_Web_Server_Servo.ino
@@ -1,22 +1,30 @@
-/*********
-  Rui Santos
-  Complete project details at http://randomnerdtutorials.com  
-*********/
+/*
+  ESP32 Remote WiFi Servo Control
+  esp32-web-servo.ino
+  Control servo motor from web page
+  
+  Based upon example from Rui Santos
+  Random Nerd Tutorials
+  https://randomnerdtutorials.com/esp32-servo-motor-web-server-arduino-ide/
+  
+
+  DroneBot Workshop 2020
+  https://dronebotworkshop.com
+*/
 
 #include <WiFi.h>
-#include <Servo.h>
+#include <ESP32Servo.h>
 
 Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
 
-// GPIO the servo is attached to
-static const int servoPin = 13;
+// Servo GPIO pin
+static const int servoPin = 27;
 
-// Replace with your network credentials
+// Network credentials
 const char* ssid     = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
-// Set web server port number to 80
+// Web server on port 80 (http)
 WiFiServer server(80);
 
 // Variable to store the HTTP request
@@ -35,9 +43,23 @@ unsigned long previousTime = 0;
 const long timeoutTime = 2000;
 
 void setup() {
+  
+  // Allow allocation of all timers for servo library
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  
+  // Set servo PWM frequency to 50Hz
+  myservo.setPeriodHertz(50);
+  
+  // Attach to servo and define minimum and maximum positions
+  // Modify as required
+  myservo.attach(servoPin,500, 2400);
+  
+  // Start serial
   Serial.begin(115200);
 
-  myservo.attach(servoPin);  // attaches the servo on the servoPin to the servo object
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
@@ -56,14 +78,24 @@ void setup() {
 }
 
 void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
+  
+  // Listen for incoming clients
+  WiFiClient client = server.available();   
+  
+  // Client Connected
+  if (client) {                             
+    // Set timer references
     currentTime = millis();
     previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+    
+    // Print to serial port
+    Serial.println("New Client."); 
+    
+    // String to hold data from client
+    String currentLine = ""; 
+    
+    // Do while client is cponnected
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { 
       currentTime = millis();
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
@@ -73,51 +105,70 @@ void loop(){
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
+        
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK) and a content-type
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
 
             // Display the HTML web page
+            
+            // HTML Header
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}");
-            client.println(".slider { width: 300px; }</style>");
+            
+            // CSS - Modify as desired
+            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto; }");
+            client.println(".slider { -webkit-appearance: none; width: 300px; height: 25px; border-radius: 10px; background: #ffffff; outline: none;  opacity: 0.7;-webkit-transition: .2s;  transition: opacity .2s;}");
+            client.println(".slider::-webkit-slider-thumb {-webkit-appearance: none; appearance: none; width: 35px; height: 35px; border-radius: 50%; background: #ff3410; cursor: pointer; }</style>");
+            
+            // Get JQuery
             client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
                      
-            // Web Page
-            client.println("</head><body><h1>ESP32 with Servo</h1>");
-            client.println("<p>Position: <span id=\"servoPos\"></span></p>");          
+            // Page title
+            client.println("</head><body style=\"background-color:#70cfff;\"><h1 style=\"color:#ff3410;\">Servo Control</h1>");
+            
+            // Position display
+            client.println("<h2 style=\"color:#ffffff;\">Position: <span id=\"servoPos\"></span>&#176;</h2>"); 
+                     
+            // Slider control
             client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\""+valueString+"\"/>");
             
+            // Javascript
             client.println("<script>var slider = document.getElementById(\"servoSlider\");");
             client.println("var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;");
             client.println("slider.oninput = function() { slider.value = this.value; servoP.innerHTML = this.value; }");
             client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
             client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>");
-           
+            
+            // End page
             client.println("</body></html>");     
             
-            //GET /?value=180& HTTP/1.1
+            // GET data
             if(header.indexOf("GET /?value=")>=0) {
               pos1 = header.indexOf('=');
               pos2 = header.indexOf('&');
+              
+              // String with motor position
               valueString = header.substring(pos1+1, pos2);
               
-              //Rotate the servo
+              // Move servo into position
               myservo.write(valueString.toInt());
+              
+              // Print value to serial monitor
+              Serial.print("Val =");
               Serial.println(valueString); 
             }         
             // The HTTP response ends with another blank line
             client.println();
+            
             // Break out of the while loop
             break;
-          } else { // if you got a newline, then clear currentLine
+          
+          } else { 
+            // New lline is received, clear currentLine
             currentLine = "";
           }
         } else if (c != '\r') {  // if you got anything else but a carriage return character,
